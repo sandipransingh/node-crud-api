0d383726509332c0550ab3f799f3a38c18c639eb
sandipransingh <sandipk.ransingh44@gmail.com>
2023-10-16 00:05:54 +0530

    first commit

---
+++ model/userModel.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+// db schema
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    mobile: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    isActive: {
+        type: Boolean,
+        default: true
+    }
+},{
+    collection: "Users",
+    timestamps: true
+})
+
+module.exports = mongoose.model("User", UserSchema)
